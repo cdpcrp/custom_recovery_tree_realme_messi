4e10e16b966c25bb0343a28a3f9d89809a3fe92d
Carlo Dee <carlodee.official@proton.me>
2024-01-11 08:27:54 +0000

    messi: Minor changes in TWRP-specific flags.
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -120,10 +120,16 @@ PLATFORM_SECURITY_PATCH := $(VENDOR_SECURITY_PATCH)
 # TWRP Configuration
 TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
-TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+TW_INCLUDE_RESETPROP := true
 TW_INCLUDE_REPACKTOOLS := true
+TW_INCLUDE_NTFS_3G := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_MAX_BRIGHTNESS := 2047
+TW_DEFAULT_BRIGHTNESS := 900
+TW_NO_SCREEN_BLANK := true
+TW_INCLUDE_FUSE_EXFAT := true
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXCLUDE_APEX := true
 TW_NO_FLASH_CURRENT_TWRP := true # For A/B devices that has dedicated recovery, removing that option in Advance.
