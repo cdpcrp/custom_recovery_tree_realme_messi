d8c71cd9f575f60677113f1ff3a466b551fdaffd
Carlo Dee <carlodee.official@proton.me>
2024-01-23 21:43:59 +0800

    messi: Removed TW_INCLUDE_REPACKTOOLS.
    * Device with dedicated recovery partition doesn't need this flag. Basically, repacktools are only needed for recovery-as-boot (now also recovery-as-vendor_boot), as it only need to flash the ramdisk, needs an unpack-repack method.
    
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -137,7 +137,6 @@ TW_EXTRA_LANGUAGES := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
 TW_INCLUDE_RESETPROP := true
-TW_INCLUDE_REPACKTOOLS := true
 TW_INCLUDE_NTFS_3G := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_MAX_BRIGHTNESS := 2047
